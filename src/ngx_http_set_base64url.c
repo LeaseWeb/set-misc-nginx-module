21da4c5410cf18bbe040c097eec2ea2adf407440
Jeffrey Kriegsman <j.kriegsman@tech.leaseweb.com>
2016-02-16 14:49:08 +0100

    Fix compilation under v1.9.11

--- src/ngx_http_set_base64url.c
+++ src/ngx_http_set_base64url.c
@@ -11,7 +11,6 @@ ngx_int_t
 ngx_http_set_misc_set_decode_base64url(ngx_http_request_t *r, ngx_str_t *res,
     ngx_http_variable_value_t *v)
 {
-    u_int     i;
     ngx_str_t src;
 
     src.len = v->len;
@@ -30,6 +29,7 @@ ngx_http_set_misc_set_decode_base64url(ngx_http_request_t *r, ngx_str_t *res,
         return NGX_ERROR;
     }
 #else
+    u_int i;
     for (i=0;i<src.len;i++) {
         switch (src.data[i]) {
             case '-': src.data[i]='+'; break;
@@ -51,7 +51,6 @@ ngx_int_t
 ngx_http_set_misc_set_encode_base64url(ngx_http_request_t *r, ngx_str_t *res,
     ngx_http_variable_value_t *v)
 {
-    u_int i;
     ngx_str_t        src;
 
     src.len = v->len;
@@ -66,6 +65,7 @@ ngx_http_set_misc_set_encode_base64url(ngx_http_request_t *r, ngx_str_t *res,
 #if defined(nginx_version) && nginx_version >= 1005010
     ngx_encode_base64url(res, &src);
 #else
+    u_int i;
     ngx_encode_base64(res, &src);
     for (i=0;i<res->len;i++) {
         switch (res->data[i]) {
